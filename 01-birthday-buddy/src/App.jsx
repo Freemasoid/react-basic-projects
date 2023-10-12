e44cab9025bbbca7822dfdb0780ce73eb0575064
Freemasoid <freemasoid@icloud.com>
2023-10-12 19:52:15 +0200

    done

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -1,4 +1,20 @@
-const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
+import { useState } from "react";
+import List from "./List";
+import data from "./data";
+
+function App() {
+  const [users, setUsers] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{users.length} birthdays today</h3>
+        <List users={users} />
+        <button className="btn btn-block" type="button" onClick={() => setUsers([])}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+}
 export default App;
