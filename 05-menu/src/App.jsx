53188fc84cfc19dbbec5aca055514f3aa1af10ec
Freemasoid <freemasoid@icloud.com>
2023-10-14 15:59:36 +0200

    done

--- 05-menu/src/App.jsx
+++ 05-menu/src/App.jsx
@@ -1,4 +1,35 @@
+import Title from "./Title";
+import Menu from "./Menu";
+import menu from "./data";
+import Categories from "./Categories";
+import { useState } from "react";
+
+// const tempCat = menu.map((item) => item.category);
+// const tempSet = new Set(tempCat);
+// const tempItems = ["all", ...tempSet];
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
